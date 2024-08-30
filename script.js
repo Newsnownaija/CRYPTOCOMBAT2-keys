cc4612d85dcc2950f2ac71efbc2f87664c878269
Newsnownaija <143800421+Newsnownaija@users.noreply.github.com>
2024-08-30 16:12:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -107,7 +107,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let selectedGame = null;
 
     sourceCode.addEventListener('click', () => {
-        window.open('https://github.com/ShafiqSadat/HamsterKeyGenWeb', '_blank');
+        window.open('https://t.me/atechcryptochannel', '_blank');
     });
     
 gameOptions.forEach(option => {
